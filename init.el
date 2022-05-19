a006e38a82c3cfd0c200f3fa8b9b69c10fc2bb0c
s-sap <z4ck.joe@protonmail.com>
2022-05-18 21:03:03 -0500

    small changes

--- init.el
+++ init.el
@@ -22,10 +22,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit vterm centered-window yasnippet-snippets yasnippet elfeed dashboard xelb use-package tablist spinner quilt popwin org-bullets multiple-cursors markdown-mode helm goto-chg doom-modeline auto-complete)))
+   '(w3m magit vterm centered-window yasnippet-snippets yasnippet elfeed dashboard xelb use-package tablist spinner quilt popwin org-bullets multiple-cursors markdown-mode helm goto-chg doom-modeline auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(term-color-black ((t (:foreground "#3F3F3F" :background "#2B2B2B"))))
+ '(term-color-blue ((t (:foreground "#7CB8BB" :background "#4C7073"))))
+ '(term-color-cyan ((t (:foreground "#93E0E3" :background "#8CD0D3"))))
+ '(term-color-green ((t (:foreground "#7F9F7F" :background "#9FC59F"))))
+ '(term-color-magenta ((t (:foreground "#DC8CC3" :background "#CC9393"))))
+ '(term-color-red ((t (:foreground "#AC7373" :background "#8C5353"))))
+ '(term-color-white ((t (:foreground "#DCDCCC" :background "#656555"))))
+ '(term-color-yellow ((t (:foreground "#DFAF8F" :background "#9FC59F"))))
+ '(term-default-bg-color ((t (:inherit term-color-black))))
+ '(term-default-fg-color ((t (:inherit term-color-white)))))
