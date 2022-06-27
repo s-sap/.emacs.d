7256969a01bfb08c587b84088dbfc5c2716df240
s-sap <z4ck.joe@protonmail.com>
2022-06-27 15:33:11 -0500

    Added tabs

--- init.el
+++ init.el
@@ -9,14 +9,23 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/myInit.org"))
 
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(centaur-tabs-background-color "black")
+ '(centaur-tabs-mode t nil (centaur-tabs))
+ '(centaur-tabs-modified-marker "*")
+ '(centaur-tabs-plain-icons t)
+ '(centaur-tabs-set-close-button nil)
+ '(centaur-tabs-set-icons t)
+ '(centaur-tabs-show-new-tab-button nil)
+ '(centaur-tabs-style "alternate")
  '(package-selected-packages
    (quote
-    (magit vterm projectile org-bullets multiple-cursors multi-term hydra helm elfeed doom-modeline dashboard centered-window bongo all-the-icons))))
+    (centaur-tabs yasnippet company lsp-mode magit projectile org-bullets multiple-cursors multi-term hydra helm elfeed doom-modeline dashboard centered-window bongo all-the-icons))))
 
 
 ;; (custom-set-faces
@@ -30,4 +39,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(centaur-tabs-selected ((t (:background "#151515" :foreground "dark gray" :height 0.9))))
+ '(centaur-tabs-selected-modified ((t (:background "#31343E" :foreground "light gray")))))
